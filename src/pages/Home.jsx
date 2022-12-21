2f890dde3f95ccf20d6659314b1e889d803926e7
Michael French <michael.x.french@gmail.com>
2022-12-20 23:12:12 -0600

    Got a start on the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,28 @@
-import {Text} from '@chakra-ui/react';
+import { Text, Button, Box } from "@chakra-ui/react";
 
 const Home = () => {
+  const handleClick = () => {
+    console.log("Register Button clicked")
+  }
+
   return (
-    <div className="home">
-      <Text>Home</Text>
-    </div>
+    <Box
+      height="100vh"
+      justifyContent="center"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+    >
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Text fontSize="6xl" fontFamily="monospace">
+          eHacks
+        </Text>
+        <Text mt="-22px" mb="3vh" fontSize="xl" fontFamily="monospace">
+          create awesome
+        </Text>
+      </Box>
+      <Button colorScheme="Black" variant="outline">Register</Button>
+    </Box>
   );
 };
 
