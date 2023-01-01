accfcfcd1ee6ae3620d18ac0b5c89759d32c89aa
sdelozi <sdelozi@siue.edu>
2023-01-01 16:51:58 -0600

    Not fully working, comitting to work at starbucks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import Navbar from './navbar/Navbar';
+import links from "../src/navbar/nav.json";
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
@@ -9,9 +11,11 @@ import Prizes from './pages/Prizes';
 import Schedule from './pages/Schedule';
 import Sponsors from './pages/Sponsors';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Navbar anchors={links.components} />
     <Home />
     <About />
     <Contact />
