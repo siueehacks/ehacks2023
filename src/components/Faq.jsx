1a8613799bc91a301d537d07a8a6f8fd03572693
Michael French <michael.x.french@gmail.com>
2023-02-04 18:13:25 -0600

    faqs

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -31,6 +31,21 @@ const Faq = () => {
             <h2>
               <AccordionButton>
                 <Box as="span" flex="1" textAlign="left" fontSize="xl">
+                  What is a hackathon?
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4} fontSize="lg">
+              A hackathon is an event where people come together to create
+              something new. It is a place where you can meet new people and
+              learn new skills,
+            </AccordionPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box as="span" flex="1" textAlign="left" fontSize="xl">
                   How much coding experience do I need to participate?
                 </Box>
                 <AccordionIcon />
@@ -47,6 +62,21 @@ const Faq = () => {
             <h2>
               <AccordionButton>
                 <Box as="span" flex="1" textAlign="left" fontSize="xl">
+                  Who can come?
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4} fontSize="lg">
+              Anyone age 18 or older who is currently enrolled in a US college
+              is able to attend.
+            </AccordionPanel>
+          </AccordionItem>
+
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box as="span" flex="1" textAlign="left" fontSize="xl">
                   Will there be food?
                 </Box>
                 <AccordionIcon />
@@ -56,6 +86,23 @@ const Faq = () => {
               Yes! All of the meals will be provided at no charge to the
               participants over the course of the weekend. There will also be
               plenty of snacks and drinks available throughout as well.
+            </AccordionPanel>
+          </AccordionItem>
+
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box as="span" flex="1" textAlign="left" fontSize="xl">
+                  What should I bring?
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4} fontSize="lg">
+              You should bring a laptop/something to code on, any chargers you
+              might need, and anything you would bring on a two day trip
+              (clothes, toiletries, etc.). We also recommend that you bring
+              stuff to sleep on.
             </AccordionPanel>
           </AccordionItem>
 
@@ -89,6 +136,35 @@ const Faq = () => {
               be a few bathrooms available for use. We recommend that you bring
               a change of clothes or two and any other hygiene products you need
               to freshen up through the weekend.
+            </AccordionPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box as="span" flex="1" textAlign="left" fontSize="xl">
+                  What if I don't have a team?
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4} fontSize="lg">
+              You can form a team using the #form-team channel in our{" "}
+              <a href="https://discord.gg/ehacks">Discord</a> server. We can
+              also help put you on a team the day of the event.
+            </AccordionPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box as="span" flex="1" textAlign="left" fontSize="xl">
+                  How big can my team be?
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4} fontSize="lg">
+              We do not put a restriction on team size, but we give out prizes
+              in fours. If you want, you can also work by yourself.
             </AccordionPanel>
           </AccordionItem>
         </Accordion>
